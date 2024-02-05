41bad5a718ebcd72676540644547821de12ad81b
Zaharov Andrey <zaharov.andrey161@gmail.com>
2024-02-05 20:36:32 +0300

    Added navbar logo on click main page redirect.

--- front/src/components/NavBar/NavBar.js
+++ front/src/components/NavBar/NavBar.js
@@ -1,9 +1,10 @@
 import './NavBar.css'
-
+import { useNavigate } from 'react-router-dom'
 export default function NavBar() {
+    const navigate = useNavigate()
     return (
         <div className='navbar'>
-            <h1 className='navbar__name'>Рисовальня</h1>
+            <h1 className='navbar__name' onClick={() => {navigate('/')}} style={{'cursor': 'pointer'}}>Рисовальня</h1>
         </div>
     )
 }
