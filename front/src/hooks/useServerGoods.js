30713f9c82c2e66c96c8b032cf18f7ae5c0828d7
Zaharov Andrey <zaharov.andrey161@gmail.com>
2024-02-06 01:32:25 +0300

    Added custom hook to fetch goods data.

--- front/src/hooks/useServerGoods.js
+++ front/src/hooks/useServerGoods.js
@@ -1,1 +1,41 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
+const useServerGoods = (apiEndpoint, pageSize = 10) => {
+  const [goods, setGoods] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [page, setPage] = useState(1)
+
+  useEffect(() => {
+    const fetchGoods = async () => {
+      try {
+        setLoading(true)
+
+        // Загрузка данных с сервера по указанному эндпоинту и странице
+        const response = await axios.get(apiEndpoint, {
+          params: { page, pageSize },
+        });
+        
+        const data = response.data
+
+        // Обновление состояния товаров
+        setGoods((prevGoods) => [...prevGoods, ...data])
+
+        setLoading(false)
+      } catch (error) {
+        console.error('Error fetching goods:', error)
+        setLoading(false)
+      }
+    }
+
+    fetchGoods()
+  }, [apiEndpoint, pageSize, page])
+
+  const loadMore = () => {
+    setPage((prevPage) => prevPage + 1)
+  };
+
+  return [goods, loading, loadMore]
+}
+
+export default useServerGoods
--- front/src/pages/CatalogPage.js
+++ front/src/pages/CatalogPage.js
@@ -1,21 +1,20 @@
 import GoodCard from '../components/GoodCard/GoodCard'
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+import useServerGoods from '../hooks/useServerGoods'
 
 export default function CatalogPage() {
-    const [goods, setGoods] = useState([])
-    useEffect(()=>{
-        axios.get('http://127.0.0.1:8000/api/catalog/').then((response) => {
-            setGoods(response.data)
-            console.log(response.data)
-        })
-    }, [])
+    const [goods, loading, loadMore] = useServerGoods('http://127.0.0.1:8000/api/catalog/')
 
     return (
         <div style={{'margin': '10px'}}>
             {goods?.map((good, i) => (
                 <GoodCard key={good.id} good_name={good.name} good_date={good.date_created} good_price={good.price_roubles} good_photo={good.image_url}></GoodCard>
             ))}
+            {loading && <p>Загрузка...</p>}
+            {!loading && (
+                <button onClick={loadMore} disabled={loading}>
+                Load More
+                </button>
+            )}
         </div>
     )
 }
