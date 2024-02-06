af97c4150063e94e0d362542a5786112da547c4c
Zaharov Andrey <zaharov.andrey161@gmail.com>
2024-02-06 04:46:32 +0300

    Removed extra char.

--- front/src/index.js
+++ front/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 import { UserProvider } from './contexts/UserContext';
-import { CartProvider } from './contexts/CartContext';}
+import { CartProvider } from './contexts/CartContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <UserProvider>
